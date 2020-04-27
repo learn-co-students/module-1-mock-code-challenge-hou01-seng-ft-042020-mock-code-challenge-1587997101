74ca46a8d25217d24e4f251bab2403489ba7f7d3
github username <github email address>
2020-04-27 11:28:57 -0400

    thach

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,26 @@
 class Student
+attr_accessor :name
+@@all = []
+
+def initialize(name)
+    @name = name 
+    @@all << self 
 
 end
+
+def self.all
+    @@all
+end 
+
+def new_course(tutor, course_name)
+    Course.new(tutor, self, course_name)
+end 
+
+def courses
+    Course.all.select {|course| course.student == self}
+end 
+
+def tutors
+    courses.map(&:tutor)
+end 
+end 
--- app/models/tutor.rb
+++ app/models/tutor.rb
@@ -1,3 +1,25 @@
 class Tutor
-  
+attr_accessor :name 
+@@all = []
+
+def initialize(name)
+    @name = name
+    @@all << self 
+end 
+def self.all
+    @@all
+end 
+
+def new_course(student, course_name)
+    Course.new(self, student, course_name)
+end 
+
+def courses
+    Course.all.select {|course| course.tutor == self}
+end 
+
+def students
+    courses.map(&:student)
+end 
+
 end
